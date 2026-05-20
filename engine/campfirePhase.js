7626dd7e417006a50ed95c47a414d2bc2978ac8c
blehwouldntdothat <dancemachineannisah@gmail.com>
2026-05-20 01:50:35 +0100

    Create campfirePhase.js

---
+++ engine/campfirePhase.js
@@ -0,0 +1,11 @@
+import { cast } from "../data/cast.js";
+
+export function runCampfirePhase(state) {
+  state.log.push(`--- CAMPFIRE SUMMARY (${state.biome}) ---`);
+
+  const alive = cast.filter(c => c.alive);
+  const dead = cast.filter(c => !c.alive);
+
+  state.log.push(`Alive: ${alive.map(a => a.name).join(", ")}`);
+  state.log.push(`Dead: ${dead.map(d => d.name).join(", ")}`);
+}
