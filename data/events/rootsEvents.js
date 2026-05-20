8d67129069f97266466405838a9937e4cf11d8a0
blehwouldntdothat <dancemachineannisah@gmail.com>
2026-05-20 02:02:17 +0100

    Create rootsEvents.js

---
+++ data/events/rootsEvents.js
@@ -0,0 +1,25 @@
+export const rootsDayEvents = [
+  (player) => ({
+    text: `${player.name} trips over a massive root and barely catches themselves.`,
+    image: player.image,
+    killed: false
+  }),
+  (player) => ({
+    text: `${player.name} uses the tangled roots as handholds to climb.`,
+    image: player.image,
+    killed: false
+  })
+];
+
+export const rootsNightEvents = [
+  (player) => ({
+    text: `${player.name} feels the forest closing in around them.`,
+    image: player.image,
+    killed: false
+  }),
+  (player) => ({
+    text: `${player.name} hears creaking branches and snaps awake.`,
+    image: player.image,
+    killed: false
+  })
+];
