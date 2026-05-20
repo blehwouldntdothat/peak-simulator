82a5c2f6d4daac8055a511a42a8c76112bdd06fe
blehwouldntdothat <dancemachineannisah@gmail.com>
2026-05-20 21:48:51 +0100

    Update shoreEvents.js

--- src/data/events/shoreEvents.js
+++ src/data/events/shoreEvents.js
@@ -4,21 +4,21 @@ export const shoreDayEvents = [
   (player, biome) => ({
     text: `${player.name} gets hit in the back with a Flying Disc.`,
     image: player.image,
-    extraImage: "images/shore/Flying_Disc.png",
+    extraImage: "images/items/Flying_Disc.png",
     killed: false
   }),
 
   (player, biome) => ({
     text: `${player.name} explores the shore and begins collecting Conches.`,
     image: player.image,
-    extraImage: "images/shore/Conch.png",
+    extraImage: "images/items/Conch.png",
     killed: false
   }),
 
   (player, biome) => ({
     text: `${player.name} hugs Bing Bong out of fear.`,
     image: player.image,
-    extraImage: "images/shore/Bing_Bong.png",
+    extraImage: "images/items/Bing_Bong.png",
     killed: false
   }),
 
@@ -26,7 +26,7 @@ export const shoreDayEvents = [
   (player, biome) => ({
     text: `${player.name} slips off the mountain after coming into contact with a jellyfish.`,
     image: player.image,
-    extraImage: "images/shore/Jellyfish.png",
+    extraImage: "images/hazards/Jellyfish.png",
     killed: false
   }),
 
@@ -34,7 +34,7 @@ export const shoreDayEvents = [
   (player, biome) => ({
     text: `${player.name} is poisoned by an urchin and tumbles off the mountain.`,
     image: player.image,
-    extraImage: "images/shore/Urchin.png",
+    extraImage: "images/hazards/Urchin.png",
     killed: true
   })
 ];
@@ -45,21 +45,14 @@ export const shoreNightEvents = [
   (player, biome) => ({
     text: `${player.name} gets hit in the back with a Flying Disc.`,
     image: player.image,
-    extraImage: "images/shore/Flying_Disc.png",
-    killed: false
-  }),
-
-  (player, biome) => ({
-    text: `${player.name} explores the shore and begins collecting Conches.`,
-    image: player.image,
-    extraImage: "images/shore/Conch.png",
+    extraImage: "images/items/Flying_Disc.png",
     killed: false
   }),
 
   (player, biome) => ({
     text: `${player.name} hugs Bing Bong out of fear.`,
     image: player.image,
-    extraImage: "images/shore/Bing_Bong.png",
+    extraImage: "images/items/Bing_Bong.png",
     killed: false
   }),
 
@@ -67,7 +60,7 @@ export const shoreNightEvents = [
   (player, biome) => ({
     text: `${player.name} slips off the mountain after coming into contact with a jellyfish.`,
     image: player.image,
-    extraImage: "images/shore/Jellyfish.png",
+    extraImage: "images/hazards/Jellyfish.png",
     killed: false
   }),
 
@@ -75,7 +68,7 @@ export const shoreNightEvents = [
   (player, biome) => ({
     text: `${player.name} is poisoned by an urchin and tumbles off the mountain.`,
     image: player.image,
-    extraImage: "images/shore/Urchin.png",
+    extraImage: "images/hazards/Urchin.png",
     killed: true
   })
 ];
