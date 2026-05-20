3f6ef7f78aca1c0fad3b0fdad801a9438f01b876
blehwouldntdothat <dancemachineannisah@gmail.com>
2026-05-20 01:50:59 +0100

    Create placements.js

---
+++ engine/placements.js
@@ -0,0 +1,11 @@
+import { cast } from "../data/cast.js";
+
+export function computePlacements() {
+  const alive = cast.filter(c => c.alive);
+  const dead = cast.filter(c => !c.alive);
+
+  alive.sort((a, b) => a.name.localeCompare(b.name));
+  dead.sort((a, b) => a.deathOrder - b.deathOrder);
+
+  return [...alive, ...dead];
+}
