b93b934d8f5a3742810ecc40432db6c6c215c2f0
blehwouldntdothat <dancemachineannisah@gmail.com>
2026-05-20 02:01:17 +0100

    Add shore events for day and night scenarios

---
+++ data/events/shoreEvents.js
@@ -0,0 +1,25 @@
+export const shoreDayEvents = [
+  (player) => ({
+    text: `${player.name} tests their footing on the wet rocks at the shore.`,
+    image: player.image,
+    killed: false
+  }),
+  (player) => ({
+    text: `${player.name} slips on seaweed and nearly falls into the water.`,
+    image: player.image,
+    killed: false
+  })
+];
+
+export const shoreNightEvents = [
+  (player) => ({
+    text: `${player.name} listens to the waves crashing in the dark.`,
+    image: player.image,
+    killed: false
+  }),
+  (player) => ({
+    text: `${player.name} is startled by something moving in the tide pools.`,
+    image: player.image,
+    killed: false
+  })
+];
