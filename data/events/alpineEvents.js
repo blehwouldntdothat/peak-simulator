a31fef372d1c1d0ee345aaeb54b0fabbe9488574
blehwouldntdothat <dancemachineannisah@gmail.com>
2026-05-20 02:02:33 +0100

    Create alpineEvents.js

---
+++ data/events/alpineEvents.js
@@ -0,0 +1,25 @@
+export const alpineDayEvents = [
+  (player) => ({
+    text: `${player.name} struggles through thin alpine air, each step heavier than the last.`,
+    image: player.image,
+    killed: false
+  }),
+  (player) => ({
+    text: `${player.name} slips on ice but manages to regain balance.`,
+    image: player.image,
+    killed: false
+  })
+];
+
+export const alpineNightEvents = [
+  (player) => ({
+    text: `${player.name} shivers in the alpine cold, teeth chattering.`,
+    image: player.image,
+    killed: false
+  }),
+  (player) => ({
+    text: `${player.name} watches distant lightning dance across the peaks.`,
+    image: player.image,
+    killed: false
+  })
+];
