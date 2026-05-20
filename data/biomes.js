53b9183f9ae26d4efa106621bf310bce416e3cea
blehwouldntdothat <dancemachineannisah@gmail.com>
2026-05-20 01:48:28 +0100

    Create biomes.js

---
+++ data/biomes.js
@@ -0,0 +1,19 @@
+export const BIOMES = {
+  SHORE: "shore",
+  TROPICS: "tropics",
+  ROOTS: "roots",
+  ALPINE: "alpine",
+  MESA: "mesa",
+  CALDERA: "caldera",
+  KILN: "kiln",
+  PEAK: "peak"
+};
+
+export const BIOME_ORDER = [
+  BIOMES.SHORE,
+  "BIOME_2",
+  "BIOME_3",
+  BIOMES.CALDERA,
+  BIOMES.KILN,
+  BIOMES.PEAK
+];
