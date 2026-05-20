d1b794d0542cf512561034b933ad469be82436e7
blehwouldntdothat <dancemachineannisah@gmail.com>
2026-05-20 20:41:41 +0100

    Update campfirePhase.js

--- src/engine/campfirePhase.js
+++ src/engine/campfirePhase.js
@@ -7,12 +7,14 @@ export function runCampfirePhase(state) {
   const dead = cast.filter(c => !c.alive);
 
   state.log.push({
-    text: `Alive: ${alive.length ? alive.map(a => a.name).join(", ") : "None"}`,
-    image: alive[0]?.image || ""
+    text: "Alive:",
+    image: "",
+    group: alive
   });
 
   state.log.push({
-    text: `Dead: ${dead.length ? dead.map(d => d.name).join(", ") : "None"}`,
-    image: dead[0]?.image || ""
+    text: "Dead:",
+    image: "",
+    group: dead
   });
 }
