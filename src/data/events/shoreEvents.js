d6dcde861e139f47a0a6eb7941ed724846b7b9db
blehwouldntdothat <dancemachineannisah@gmail.com>
2026-05-21 01:45:36 +0100

    Update shoreEvents.js

--- src/data/events/shoreEvents.js
+++ src/data/events/shoreEvents.js
@@ -1,80 +1,197 @@
 export const shoreDayEvents = [
   (player, biome) => ({
-    text: `${player.name} gets hit in the back with a Flying Disc.`,
+    text: `${player.name} gets hit in the back with a flying disc.`,
     image: player.image,
     extraImage: "images/items/Flying_Disc.png",
     killed: false
   }),
-
   (player, biome) => ({
-    text: `${player.name} explores the shore and begins collecting Conches.`,
+    text: `${player.name} explores the shore and begins collecting conches.`,
     image: player.image,
     extraImage: "images/items/Conch.png",
     killed: false
   }),
-
   (player, biome) => ({
     text: `${player.name} hugs Bing Bong out of fear.`,
     image: player.image,
     extraImage: "images/items/Bing_Bong.png",
     killed: false
   }),
-
   (player, biome) => ({
     text: `${player.name} slips off the mountain after coming into contact with a jellyfish.`,
     image: player.image,
     extraImage: "images/hazards/Jellyfish.png",
     killed: true
   }),
-
   (player, biome) => ({
     text: `${player.name} is poisoned by an urchin and tumbles off the mountain.`,
     image: player.image,
     extraImage: "images/hazards/Urchin.png",
     killed: true
   }),
-    
   (player, biome) => ({
     text: `${player.name} keels over after eating one too many green crispberries.`,
     image: player.image,
     extraImage: "images/items/Green_Crispberry.png",
     killed: true
-  })
+  }),
+  (player, biome) => ({
+    text: `${player.name} stashes a bunch of red crispberries.`,
+    image: player.image,
+    extraImage: "images/items/Red_Crispberry.png",
+    killed: false
+  }),
+  (player, biome) => ({
+    text: `${player.name} feels energised after eating yellow crispberries.`,
+    image: player.image,
+    extraImage: "images/items/Yellow_Crispberry.png",
+    killed: false
+  }),
+  (player, biome) => ({
+    text: `${player.name} plays with the lantern left by the crash site.`,
+    image: player.image,
+    extraImage: "images/items/Lantern.png",
+    killed: false
+  }),
+  (player, biome) => ({
+    text: `${player.name} keeps the flare on their person, just in case.`,
+    image: player.image,
+    extraImage: "images/items/Flare.png",
+    killed: false
+  }),
+  (player, biome) => ({
+    text: `${player.name} attempts to improve morale by playing a beautifully off-tempo song with the bugle.`,
+    image: player.image,
+    extraImage: "images/items/Bugle.png",
+    killed: false
+  }),
+  (player, biome) => ({
+    text: `${player.name} reads the scout guidebook, just in case.`,
+    image: player.image,
+    extraImage: "images/items/Guidebook.png",
+    killed: false
+  }),
+  (player, biome) => ({
+    text: `${player.name} eats a medicinal root to heal their injuries.`,
+    image: player.image,
+    extraImage: "images/items/Medicinal_Root.png",
+    killed: false
+  }),
+  (player, biome) => ({
+    text: `${player.name} eats a coconut.`,
+    image: player.image,
+    extraImage: "images/items/Coconut.png",
+    killed: false
+  }),
+  (player, biome) => ({
+    text: `${player.name} gets hit in the head by a flying coconut.`,
+    image: player.image,
+    extraImage: "images/items/Half-Coconut.png",
+    killed: false
+  }),
+  (player, biome) => ({
+    text: `${player.name} anxiously crosses a flimsy, yet intact bridge.`,
+    image: player.image,
+    extraImage: "images/hazards/BridgeShore.png",
+    killed: false
+  }),
 ];
 
 export const shoreNightEvents = [
   (player, biome) => ({
-    text: `${player.name} gets hit in the back with a Flying Disc.`,
+    text: `${player.name} gets hit in the back with a flying disc.`,
     image: player.image,
     extraImage: "images/items/Flying_Disc.png",
     killed: false
   }),
-
+  (player, biome) => ({
+    text: `${player.name} explores the shore and begins collecting conches.`,
+    image: player.image,
+    extraImage: "images/items/Conch.png",
+    killed: false
+  }),
   (player, biome) => ({
     text: `${player.name} hugs Bing Bong out of fear.`,
     image: player.image,
     extraImage: "images/items/Bing_Bong.png",
     killed: false
   }),
-
   (player, biome) => ({
     text: `${player.name} slips off the mountain after coming into contact with a jellyfish.`,
     image: player.image,
     extraImage: "images/hazards/Jellyfish.png",
-    killed: false
+    killed: true
   }),
-
   (player, biome) => ({
     text: `${player.name} is poisoned by an urchin and tumbles off the mountain.`,
     image: player.image,
     extraImage: "images/hazards/Urchin.png",
     killed: true
   }),
-    
   (player, biome) => ({
     text: `${player.name} keels over after eating one too many green crispberries.`,
     image: player.image,
-    extraImage: "images/hazards/Green_Crispberry.png",
+    extraImage: "images/items/Green_Crispberry.png",
     killed: true
-  })
+  }),
+  (player, biome) => ({
+    text: `${player.name} stashes a bunch of red crispberries.`,
+    image: player.image,
+    extraImage: "images/items/Red_Crispberry.png",
+    killed: false
+  }),
+  (player, biome) => ({
+    text: `${player.name} feels energised after eating yellow crispberries.`,
+    image: player.image,
+    extraImage: "images/items/Yellow_Crispberry.png",
+    killed: false
+  }),
+  (player, biome) => ({
+    text: `${player.name} plays with the lantern left by the crash site.`,
+    image: player.image,
+    extraImage: "images/items/Lantern.png",
+    killed: false
+  }),
+  (player, biome) => ({
+    text: `${player.name} keeps the flare on their person, just in case.`,
+    image: player.image,
+    extraImage: "images/items/Flare.png",
+    killed: false
+  }),
+  (player, biome) => ({
+    text: `${player.name} attempts to improve morale by playing a beautifully off-tempo song with the bugle.`,
+    image: player.image,
+    extraImage: "images/items/Bugle.png",
+    killed: false
+  }),
+  (player, biome) => ({
+    text: `${player.name} reads the scout guidebook, just in case.`,
+    image: player.image,
+    extraImage: "images/items/Guidebook.png",
+    killed: false
+  }),
+  (player, biome) => ({
+    text: `${player.name} eats a medicinal root to heal their injuries.`,
+    image: player.image,
+    extraImage: "images/items/Medicinal_Root.png",
+    killed: false
+  }),
+  (player, biome) => ({
+    text: `${player.name} eats a coconut.`,
+    image: player.image,
+    extraImage: "images/items/Coconut.png",
+    killed: false
+  }),
+  (player, biome) => ({
+    text: `${player.name} gets hit in the head by a flying coconut.`,
+    image: player.image,
+    extraImage: "images/items/Half-Coconut.png",
+    killed: false
+  }),
+  (player, biome) => ({
+    text: `${player.name} anxiously crosses a flimsy, yet intact bridge.`,
+    image: player.image,
+    extraImage: "images/hazards/BridgeShore.png",
+    killed: false
+  }),
 ];
