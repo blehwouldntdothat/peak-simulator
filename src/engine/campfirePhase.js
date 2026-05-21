329b4bae2df9017d67a72e3c375b1aec2f8654e6
blehwouldntdothat <dancemachineannisah@gmail.com>
2026-05-21 17:47:59 +0100

    Update campfirePhase.js

--- src/engine/campfirePhase.js
+++ src/engine/campfirePhase.js
@@ -3,6 +3,19 @@ import { cast } from "../data/cast.js";
 export function runCampfirePhase(state) {
   state.log = [];
 
+  // STATUE REVIVAL
+  if (state.statueEnabled && cast.some(c => !c.alive)) {
+    cast.forEach(c => {
+      c.alive = true;
+      c.deathOrder = null;
+    });
+
+    state.log.push({
+      text: "A loud rumble tears through an ancient, mystical statue. All deceased scouts have been revived.",
+      group: []
+    });
+  }
+
   const alive = cast.filter(c => c.alive);
   const dead = cast.filter(c => !c.alive);
 
